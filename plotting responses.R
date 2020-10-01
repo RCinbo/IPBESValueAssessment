4981b36c1cc1b11439acbf10b7a467f2e1241250
CARMEN <raisa.carmen@inbo.be>
2020-10-01 12:02:13 +0200

    Added code to make a figure based on table 3 - Q2.17 & Q2.18. Also added code to write an excel with all papers that are not Desired uptake.

--- plotting responses.R
+++ plotting responses.R
@@ -1,1 +1,54 @@
 # HL new script test
+library(ggplot2)
+library(scales)
+library(stringr)
+library(tidyverse)
+library(grDevices)
+library(writexl)
+############Define a color scheme#####################
+IPbesdarkgreen <- rgb(92/255, 102/255, 93/255) #5c665d
+IPbeslightgreen <- rgb(181/255, 212/255, 141/255) #b5d48d
+colfunc <- colorRampPalette(c(rgb(92/255, 102/255, 93/255), rgb(181/255, 212/255, 141/255)))
+
+####------Question 2.17 and 2.18 stacked bar charts----------####
+# First, we need to disentangle the options in 2.18
+legend<-data.frame(code=c('Inf_FA','Inf_AR','Inf_Just','Inf_AI','Dec_RG','Dec_Partic','Dec_PT','Dec_EMC','T_PS','T_D','Unclear', 'Other'),
+                   txt=c('Informative - Formative or Affirmative',
+                         'Informative - Awareness raising',
+                         'Informative - Justification',
+                         'Informative - Accounting & Indicators',
+                         'Decisive - Recommendations & guidance',
+                         'Decisive  - Participation',
+                         'Decisive - Prioritization on Trade-offs',
+                         'Decisive - Environmental management criterion',
+                         'Technical - Price-setting',
+                         'Technical  - Damage compensation',
+                         'unclear',
+                         'Other'),
+                    Fulltxt=c('Informative - Formative or Affirmative (Value formation or affirmation (not used
+decisively or for technical purposes))', 'Informative - Awareness raising (Advocacy and raising awareness of total value, trade-offs, conflicts, scenarios)','Informative - Justification (ex post of a decision, Evaluation of existing projects and policies ex post)','Informative - Accounting & Indicators (Assessment of historic trends)','Decisive - Recommendations & guidance (Guidances, strategies, plans)','Decisive - Participation (Negotiation, arguments for discussion, shared norms & conflict resolution // Formulation of decision problem and structuring)','Decisive - Prioritization on Trade-offs (screening alternatives // ranking alternatives)', 'Decisive - Environmental management criterion (Policy target-setting // Criteria for spatial targeting (zoning, planning) // Allocation of rights to land and natural resource use)','Technical - Price-setting (Environmental standard setting (implicit pricing) // Pricing, setting incentive levels (explicit pricing))', 'Technical - Damage compensation (Establishing levels of damage compensation)', 'unclear', 'Other'))
+subset<-s3_single[,c('paperID','2.17','2.18')]
+for(i in 1:nrow(legend)){
+  A <-  str_detect(subset$`2.18`, pattern = as.character(legend[i,'txt']))
+  subset <- cbind(subset, A)
+  colnames(subset)[ncol(subset)] <- as.character(legend[i,'code'])
+}
+#from wide to long format
+subset %>%
+  gather(question, value, -paperID,-`2.17`,-`2.18`) ->subset_long
+#labels for the facets
+labs <- c(sprintf('Desired uptake, N=%d', sum(subset$`2.17`=='Desired uptake - A potential, expected, or wished for use of outputs, sometimes only cursory')),
+          sprintf('Testing use case, N=%d', sum(subset$`2.17`=='Testing use case - Researcher initiated valuation study, reported test of use to enable decision making')),
+          sprintf('Actual Use case, N=%d', sum(subset$`2.17`=='Actual use case - Stakeholder commissioned valuation study, reported use to enable decision making')))
+names(labs) <- c('Desired uptake - A potential, expected, or wished for use of outputs, sometimes only cursory', 'Testing use case - Researcher initiated valuation study, reported test of use to enable decision making', 'Actual use case - Stakeholder commissioned valuation study, reported use to enable decision making')
+#--------making the plot-----#
+p <- ggplot(subset_long) + geom_bar(alpha=1,size=1,color = IPbeslightgreen, fill = IPbeslightgreen,aes(x=question, y=1*value, group = `2.17` ), stat = "identity")+ facet_grid(cols = vars(`2.17`), scales = "free", labeller = labeller(`2.17` = labs)) + coord_flip() + theme_bw() + scale_x_discrete(breaks = legend$code, labels = str_wrap(as.character(legend$txt),20)) + ylab('Number of papers') + xlab('Valuation results purpose')
+ggsave(p, filename='output/T3-Q217-218.pdf', width= 10, height = 7)
+#extract paper ID numbers for testing use case and actual use case and give the purposes that were given for this application
+  write_xlsx(
+    subset[subset$`2.17`!='Desired uptake - A potential, expected, or wished for use of outputs, sometimes only cursory', -3],
+    path =  "output/testing and actual use cases.xlsx",
+    col_names = TRUE,
+    format_headers = TRUE,
+    use_zip64 = FALSE
+  )
